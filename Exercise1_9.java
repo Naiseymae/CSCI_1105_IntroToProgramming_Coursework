55c03e7f19face37332cdeece02f905f3b4ea4d3
Renee Linford <48326687+Naiseymae@users.noreply.github.com>
2019-03-07 16:29:12 -0700

    Add Chapter 1 Exercises
    Exercise 1-5
    Exercise 1-9
    Exercise 1-13

---
+++ Exercise1_9.java
@@ -0,0 +1,18 @@
+
+public class Exercise1_9 {
+
+	// Programming Exercise 1-9
+	public static void main(String[] args) {
+		
+		System.out.println("Width = 4.5");
+		System.out.println("Height = 7.9");
+		System.out.println("Area = Width * Height");
+		System.out.print("Area = ");
+		System.out.println(4.5 * 7.9);
+		System.out.println("Perimeter = Width + Width + Height + Height");
+		System.out.print("Perimeter = ");
+		System.out.println(4.5 + 4.5 + 7.9 + 7.9);
+
+	}
+
+}
